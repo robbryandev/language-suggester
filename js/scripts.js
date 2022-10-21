dea31d273e634f9f082a328addb62c7b77483963
robbryandev <robbryandev@gmail.com>
2022-10-21 09:51:03 -0700

    Auto switch last question groups

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,14 @@
 // Business
 // narrowed [nim, python]
-function langType(opt) {
-  const interpreted = ["Ruby", "JavaScript", "Python"];
+function langType() {
+  const comped = parseInt(document.getElementById("lang-type").value);
   const compiled = ["C#", "Nim", "Rust", "Swift"];
+  const interpreted = ["Ruby", "JavaScript", "Python"];
 
-  if (opt === "1") {
-    return interpreted;
+  if (Boolean(comped)) {
+    return {"comped": true, "list": compiled};
   } else {
-    return compiled;
+    return {"comped": false, "list": interpreted};
   }
 }
 
@@ -20,8 +21,41 @@ function tabbed(list) {
   }
 }
 
+function isTabbed() {
+  const tabInput = parseInt(document.getElementById("tabbed").value);
+  return Boolean(tabInput);
+}
 
 // Ui
-addEventListener("load", function() {
+function hideOther() {
+  let other = this.document.querySelectorAll(".other");
+  for (let o = 0; o < other.length; o++) {
+    other[o].classList.add("invisible");
+  }
+}
 
+function show() {
+  hideOther();
+  let show;
+  console.log(`Lang Type: ${langType().comped}`)
+  if (langType().comped) {
+    show = this.document.querySelectorAll(".comp");
+  } else {
+    show = this.document.querySelectorAll(".inter");
+  }
+
+  for (let s = 0; s < show.length; s++) {
+    show[s].classList.remove("invisible");
+  }
+}
+
+addEventListener("load", function() {
+  const form = this.document.querySelector("form");
+  this.setInterval(() => {
+    if (isTabbed()) {
+      hideOther();
+      return;
+    }
+    show();
+  }, 500)
 })
